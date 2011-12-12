c9e8419a79a7e8094ad63e9df002ac55e57cb1dd
Russ Smith <russ@bashme.org>
2011-12-11 20:39:01 -0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,26 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.3'
+gem 'haml', '3.0.25'
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'biceps'
+
+group :assets do
+  gem 'sass-rails', '~> 3.1.5'
+  gem 'coffee-rails', '~> 3.1.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'rspec-rails', '2.6.1'
+  gem 'rspec', '2.6.0'
+  gem 'factory_girl_rails', '~> 1.4'
+  gem 'shoulda-matchers'
+end
+
+group :production do
+  gem 'mysql2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jshq::Application.config.secret_token = 'c944d54e57434f6b71bcced0de84b60496fdc3faa90a34284b87e67210a6d93afec313b9635dd3fd45d8be53c46d9e88154c7daef90945a8494b52f0a303317f'
